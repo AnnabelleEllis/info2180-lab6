fccb77c06e6bebe9129f95255c391660adacc49d
Annabelle Ellis <annabelle.ellis@mymona.uwi.edu>
2018-11-23 23:16:23 +0000

    My Second Commit

--- request.js
+++ request.js
@@ -1,10 +1,38 @@
 window.onload = function(){
+  
+var allDef = document.getElementById("allDef");
 var message = document.getElementById("search");
+
+allDef.addEventListener("click",function(){
+    console.log(" All Definitions Button CLicked!");
+    
+    
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+             var response = xhttp.responseXML;
+             var storeResults = document.querySelector('#demo2');
+             var storeAllDef = response.getElementsByTagName('definition');
+            
+            var results = document.createElement('ol');
+            allDef.appendChild(storeResults);
+            for(var i = 0; i < storeAllDef.length; i++){
+            var defList = document.createElement('li');
+            var text = document.creatTextNode(storeAllDef[i].children[0].childNode[0].nodeValues);
+              defList.appendChild(text);
+              results.append(defList);
+            }
+        
+        }
+      };
+      xhttp.open("GET", "request.php?q=&all=true", true);
+      xhttp.send();
+});
 
 message.addEventListener("click", function(e){
     e.preventDefault();
     var textField = document.getElementById("getText").value;
-    console.log("Button CLicked!")
+    console.log("Button CLicked!");
       var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -13,6 +41,6 @@ message.addEventListener("click", function(e){
       };
       xhttp.open("GET", "request.php?q="+ textField, true);
       xhttp.send();
-})
+});
 
-}
+};
--- request.php
+++ request.php
@@ -2,6 +2,46 @@
 // accept a term (keyword)
 // respond with a value
 $query = $_GET['q'];
+
+$allDefs = $_GET['all'];
+if ($allDefs == 'true'){
+    $xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries>
+ <definition name="definition" author="John">
+ a statement of the exact meaning of a word, especially in
+a dictionary.
+ </definition>
+ 
+ <definition name="bar" author="mary">
+ a place that sells alcholic beverages
+ </definition>
+ 
+ <definition name="ajax" author="Kimberly">
+ technique which involves the use of javascript and xml
+ </definition>
+ 
+ <definition name = "html" author = "Annabelle">
+ The standard markup language for creating web pages and web applications.
+ </definition>
+ 
+ <definition name = "css" author = "Nadine">
+ A style sheet language used for describing the presentation of a document written in a markup language.
+ </definition>
+ 
+ <definition name = "javascript" author = "Kaela">
+ A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+ </definition>
+ 
+ <definition name = "php" author = "Savannah">
+ A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+ </definition>
+ 
+</entries>';
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+echo $xmlOutput->asXML();
+    
+}else{
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
@@ -11,6 +51,8 @@ $definition = [
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
+
 print "<h3>" . strtoupper($query) . "</h3>";
 print "<p>" . $definition[$query] . "</p>";
+}
 ?>
